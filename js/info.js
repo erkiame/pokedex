53231bab01b857bd2e5d22223b4617f4666c5778
erki <erkiame3@gmail.com>
2021-06-07 13:33:42 +0600

    code quality impovement

---
+++ js/info.js
@@ -0,0 +1,3 @@
+const pokemons = document.querySelectorAll('.pokemon');
+const types = document.querySelectorAll('.pokemon__type');
+const abilities = document.querySelectorAll('.pokemon__ab');
--- js/script.js
+++ js/script.js
@@ -1,97 +1,59 @@
 const mainContainer = document.getElementById('main-body');
 
-let getPokemon = id => {
-    let pokemonData = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    let pokemonSpriteUrl = fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`);
+const fetchPokemons = async () => {
+    for (let index = 1; index <= 1118; index++) {
+        await getPokemon(index);
+    }
+};
 
-    let pokemon = document.createElement('div');
+const getPokemon = async (id) => {
+    const pokemonDataRes = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokemonData = await pokemonDataRes.json();
+
+    const {sprites, name, id: pokeId, abilities, height, weight, types} = pokemonData;
+
+    createPokemon(sprites, name, pokeId, abilities, height, weight, types);
+}
+
+const createPokemon = (sprites, name, id, abilities, height, weight, types) => {
+    const elements = `
+    <div class="pokemon__sprite">
+    <img src=${sprites.front_default} alt="pokemon sprite" />
+    </div>
+    <div class="pokemon__name">
+    <span>${name}</span>
+    </div>
+    <div class="pokemon__id">
+    <span>${id}</span>
+    </div>
+    <div class="pokemon__height">
+    <span>Height: ${height / 10}m</span>
+    </div>
+    <div class="pokemon__weight">
+    <span>Weight: ${weight / 10}kg</span>
+    </div>
+    <div class="pokemon__types">${types.map(type => `<span>${type.type.name}<span>`).join('')}</div>
+    <div class="pokemon__ab">${abilities.map(ab => `<span>${ab.ability.name}<span>`).join('')}</div>
+    `
+
+    const pokemon = document.createElement('div');
     pokemon.className = 'pokemon';
 
-    pokemonData.then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error status: ${response.status}`);
-        } else {
-            return response.json();
-        }
-    })
-    .then(dataJson => {
-        let {name, id, abilities, height, weight, types} = dataJson;
-        let pokemonTypes = [];
+    pokemon.insertAdjacentHTML('afterbegin', elements);
+    mainContainer.appendChild(pokemon);
 
-        types.forEach(type => pokemonTypes.push(type.type.name));
-
-        let pokemonName = document.createElement('div');
-        pokemonName.className = 'pokemon-name';
-        let pokemonNameSpan = document.createElement('span');
-        pokemonNameSpan.innerText = name;
-
-        let pokemonId = document.createElement('div');
-        pokemonId.className = 'pokemon-id';
-        let pokemonIdSpan = document.createElement('span');
-        pokemonIdSpan.innerText = id;
-
-        let pokemonHeight = document.createElement('div');
-        pokemonHeight.className = 'pokemon-height';
-        let pokemonHeightSpan = document.createElement('span');
-        pokemonHeightSpan.innerText = `Height: ${height / 10}`;
-
-        let pokemonWeight = document.createElement('div');
-        pokemonWeight.className = 'pokemon-weight';
-        let pokemonWeightSpan = document.createElement('span');
-        pokemonWeightSpan.innerText = `Weight: ${weight / 10}`;
-
-        pokemonName.appendChild(pokemonNameSpan);
-        pokemonId.appendChild(pokemonIdSpan);
-        pokemonHeight.appendChild(pokemonHeightSpan);
-        pokemonWeight.appendChild(pokemonWeightSpan);
-
-        pokemon.appendChild(pokemonName);
-        pokemon.appendChild(pokemonId);
-        pokemon.appendChild(pokemonHeight);
-        pokemon.appendChild(pokemonWeight);
-
-        pokemonTypes.forEach(type => {
-            let pokemonType = document.createElement('div');
-            pokemonType.className = 'pokemon-type';
-            let pokemonTypeSpan = document.createElement('span');
-            pokemonTypeSpan.innerText = type;
-
-            pokemonType.appendChild(pokemonTypeSpan);
-            pokemon.appendChild(pokemonType);
-        })
-
-        pokemonSearch(pokemon);
-    })
-    .catch(error => console.log(error))
-
-    pokemonSpriteUrl.then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error status: ${response.status}`);
-        } else {
-            let pokemonSprite = document.createElement('img');
-            pokemonSprite.src = response.url;
-            let pokeImageWrapper = document.createElement('div');
-            pokeImageWrapper.className = 'image-wrapper';
-            pokeImageWrapper.appendChild(pokemonSprite);
-            pokemon.appendChild(pokeImageWrapper);
-
-            mainContainer.appendChild(pokemon);
-        }
-    })
-    .catch(error => console.log(error))
-
-    const pokemonSearch = (pokemon) => {
-        search.addEventListener('keyup', () => {
-            const searchText = search.value.toString().toLowerCase();
-            if (pokemon.innerHTML.indexOf(searchText) > -1) {
-                pokemon.style.display = '';
-            } else {
-                pokemon.style.display = 'none';
-            }
-        })
-    };
+    pokemonSearch(pokemon);
 }
 
-for (let index = 1; index <= 1118; index++) {
-    getPokemon(index);
-}
+const pokemonSearch = (pokemon) => {
+    search.addEventListener('keyup', () => {
+        const searchText = search.value.toString().toLowerCase();
+        if (pokemon.innerHTML.indexOf(searchText) > -1) {
+            pokemon.style.display = '';
+        } else {
+            pokemon.style.display = 'none';
+        }
+    })
+};
+
+fetchPokemons();
